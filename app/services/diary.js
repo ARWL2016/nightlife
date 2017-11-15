24a5c1e0a9df140e0d88b807c57896b03a82daae
Alistair Willis <alistairrwillis@gmail.com>
2017-11-15 22:18:21 +0900

    resolve form post error

--- app/services/diary.js
+++ app/services/diary.js
@@ -4,8 +4,9 @@ pathFinderApp.factory('diaryService', function($http) {
 
   function addToDiary(user, location, datetime) {
     const data = JSON.stringify({user, location, datetime});
+
     const url = '/api/diary/add';
-    const config = { headers : {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'}};
+    const config = { headers : {'Content-Type': 'application/json'}};
 
     console.log({url, data});
     return $http.post(url, data, config)
--- server/controllers/diary.js
+++ server/controllers/diary.js
@@ -1,9 +1,12 @@
+
+
 const util = require('util');
 
 function addToDiary(req, res) {
   console.log('************ ADD TO DIARY **********************');
   console.log(req.body);
-  console.log(util.inspect(req, {depth: 0}));
+  // console.log(util.inspect(req, {depth: 0}));
+  
   
 }
 
