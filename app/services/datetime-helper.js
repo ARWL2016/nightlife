314341e3cda9001ee4713292e26a4d2c7ab70b80
Alistair Willis <alistairrwillis@gmail.com>
2017-11-15 16:42:25 +0900

    add diary widget

--- app/services/datetime-helper.js
+++ app/services/datetime-helper.js
@@ -8,20 +8,29 @@ pathFinderApp.factory('datetimeHelperService', function() {
   function getNextWeek() {
     const nextWeek = [];
     
-    for (let i = 0; i < 7; i++) {
+    for (let i = 1; i < 7; i++) {
       const currentDate = new Date();
       currentDate.setDate(currentDate.getDate() + i);
 
       const day = week[currentDate.getDay()];
       const date = currentDate.getDate();
       const month = months[currentDate.getMonth()];
-      const dateString = i === 0 ? 'Today' : `${day}, ${date} ${month}`; 
+      // const dateString = i === 0 ? 'Today' : `${day}, ${date} ${month}`; 
+      const dateString = `${day}, ${date} ${month}`; 
 
       nextWeek.push(dateString);
     }
     return nextWeek;
   }
+
+  function getHours() {
+    const hours = [];
+    for (let i = 2; i < 13; i++) {
+      hours.push(i + ':00');
+    }
+    return hours;
+  }
   
-  return { getNextWeek };
+  return { getNextWeek, getHours };
   
 });
